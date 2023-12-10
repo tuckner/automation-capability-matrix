e12da4bd7fb88e10400203b10ad2ce305a1b73d5
John Tuckner <tuckner329@gmail.com>
2023-12-10 10:38:25 -0600

    Updates

--- node_modules/framer-motion/dist/es/events/utils.mjs
+++
@@ -1,8 +0,0 @@
-import { isBrowser } from '../utils/is-browser.mjs';
-
-// We check for event support via functions in case they've been mocked by a testing suite.
-const supportsPointerEvents = () => isBrowser && window.onpointerdown === null;
-const supportsTouchEvents = () => isBrowser && window.ontouchstart === null;
-const supportsMouseEvents = () => isBrowser && window.onmousedown === null;
-
-export { supportsMouseEvents, supportsPointerEvents, supportsTouchEvents };
--- node_modules/framer-motion/dist/es/render/dom/value-types/type-int.mjs
+++
@@ -1,8 +0,0 @@
-import { number } from 'style-value-types';
-
-const int = {
-    ...number,
-    transform: Math.round,
-};
-
-export { int };
