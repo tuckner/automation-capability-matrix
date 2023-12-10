5b7c82235a176b868168871eac113332e413c85e
John Tuckner <tuckner329@gmail.com>
2023-12-10 10:50:55 -0600

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,15 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import eslint from 'vite-plugin-eslint'
 
-export default defineConfig({
-  server: {
-    base: "/automation-capability-matrix/",
-  },
-  plugins: [
-    tsconfigPaths(),
-    react(),
-    eslint({ cache: false})
-  ]
-})
+export default defineConfig(() => {
+  const config = {
+    base: `/automation-capability-matrix/`,
+    plugins: [
+      tsconfigPaths(),
+      react(),
+      eslint({ cache: false})
+    ]
+  };
+
+  return config;
+});
